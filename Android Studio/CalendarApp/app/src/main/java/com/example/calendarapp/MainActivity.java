386c5ac444f70713c91ef7dc03600d95475579a5
kubaxd864 <79090487+kubaxd864@users.noreply.github.com>
2023-02-16 09:16:20 +0100

    Update CalednarApp

--- Android Studio/CalendarApp/app/src/main/java/com/example/calendarapp/MainActivity.java
+++ Android Studio/CalendarApp/app/src/main/java/com/example/calendarapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.calendarapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +28,11 @@ public class MainActivity extends AppCompatActivity {
         Calendar currentDate = Calendar.getInstance();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         Date data = currentDate.getTime();
-        Now.setText(dateFormat.format(data));
-
+        String Day = LocalDate.now().getDayOfWeek().name();
+        Now.setText(Day + " " + dateFormat.format(data));
 
         Calendar BirthdayDate = Calendar.getInstance();
-        BirthdayDate.set(2023, Calendar.FEBRUARY, 13);
+        BirthdayDate.set(2024, Calendar.FEBRUARY, 13);
         long diff = BirthdayDate.getTimeInMillis() - currentDate.getTimeInMillis();
         int daysToBirthday = (int) (diff / (24 * 60 * 60 * 1000));
         Birthday.setText(daysToBirthday + " dni");
