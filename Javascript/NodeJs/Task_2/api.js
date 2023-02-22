5586694ee08f224cbc7f1d8632e6d570981cdd6f
kubaxd864 <jakubsobczyk484@gmail.com>
2023-02-22 22:04:10 +0100

    Update Task_2

--- Javascript/NodeJs/Task_2/api.js
+++ Javascript/NodeJs/Task_2/api.js
@@ -14,8 +14,8 @@ app.set('views', path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 app.get('/', (req, res) => {
-    var Error = ""
-    res.render('Bank', { Error : Error } )
+    var Message = ""
+    res.render('Bank', { Message : Message } )
 })
 
 app.post("/bank", urlencodedParser, async (req, res) => {
@@ -24,22 +24,22 @@ app.post("/bank", urlencodedParser, async (req, res) => {
         dbConnection.execute('Select * from clients WHERE FullName = ?', [req.body.name])
         .then(([rows]) => {
             if(rows.length == 0){
-                var Error = "Niepoprawne Imię i Nazwisko"
-                res.render('Bank', { Error : Error } )
+                var Message = "Niepoprawne Imię i Nazwisko"
+                res.render('Bank', { Message : Message } )
             }
             else if(rows[0].FullName == req.body.name) {
                 dbConnection.execute('Select AccountPin from accounts WHERE AccountPin = ?', [req.body.pin])
                 .then(([rows1]) => {
                     if(rows1.length == 0){
-                        var Error = "Niepoprawne Pin do Konta"
-                        res.render('Bank', { Error : Error } )
+                        var Message = "Niepoprawne Pin do Konta"
+                        res.render('Bank', { Message : Message } )
                     }
                     else if(rows1[0].AccountPin == req.body.pin) {
                         global.AccountOwnerID = rows[0].ClientID
                         global.FullName = req.body.name
                         global.Name = req.body.name.split(" ")[0]
-                        var Error = ""
-                        res.render('Menu' , { Name : global.Name, Error : Error })
+                        var Message = ""
+                        res.render('Menu' , { Name : global.Name, Message : Message })
                     }
                 })
             }
@@ -47,8 +47,8 @@ app.post("/bank", urlencodedParser, async (req, res) => {
 
     } 
      else {
-        var Error = "Nie Podano Danych"
-        res.render('Bank', { Error : Error } )
+        var Message = "Nie Podano Danych"
+        res.render('Bank', { Message : Message } )
     }
     } catch (error) {
         console.log(error)
@@ -62,11 +62,12 @@ app.post("/menu", urlencodedParser, async (req, res) => {
         dbConnection.execute('Select * from clients WHERE FullName = ?', [global.FullName])
         .then(([rows]) => {
             if(rows.length == 0){
-                console.log("Nie Można Wyświetlić Danych")
+                var Message = "Nie Można Wyświetlić Danych"
+                res.render('Menu' , { Message : Message, Name : global.Name })
             }
             else{
-                var Error = "Imię i Nazwisko: " + rows[0].FullName + " Adres: " + rows[0].Address + " Numer Telefonu: " + rows[0].Phonenumber
-                res.render('Menu' , { Error : Error, Name : global.Name })
+                var Message = "Imię i Nazwisko: " + rows[0].FullName + " Adres: " + rows[0].Address + " Numer Telefonu: " + rows[0].Phonenumber
+                res.render('Menu' , { Message : Message, Name : global.Name })
             }
         })
     }
@@ -78,25 +79,26 @@ app.post("/menu", urlencodedParser, async (req, res) => {
         var pin = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
         var AccountNumber = first_part + "" + second_part + third_part + fourth_part 
         dbConnection.execute('INSERT INTO accounts (AccountOwnerID, AccountNumber, AccountPin, Balance) VALUES (?,?,?,?)', [global.AccountOwnerID, AccountNumber, pin, 0])
-        var Error = "Dodano Konto"
-        res.render('Menu' , { Error : Error, Name : global.Name })
+        var Message = "Dodano Konto"
+        res.render('Menu' , { Message : Message, Name : global.Name })
     }
     else if (req.body.choice == "Wpłata") {
         dbConnection.execute('Select Balance from accounts WHERE AccountOwnerID = ?', [global.AccountOwnerID])
         .then(([rows]) => {
             if(rows.length == 0){
-                console.log("Nie Można Wyświetlić Danych")
+                var Message = "Nie Można Wyświetlić Danych"
+                res.render('Menu' , { Message : Message, Name : global.Name })
             }
             else{
                 if(req.body.amount <= 0){
-                    var Error = "Podaj Kwotę większą od 0"
-                    res.render('Menu' , { Error : Error, Name : global.Name })
+                    var Message = "Podaj Kwotę większą od 0"
+                    res.render('Menu' , { Message : Message, Name : global.Name })
                 }
                 else{
                     var AddBalance = (parseInt(rows[0].Balance) + parseInt(req.body.amount))
                     dbConnection.execute('UPDATE accounts SET Balance = ? WHERE AccountOwnerID = ?', [AddBalance, global.AccountOwnerID])
-                    var Error = "Wpłacono"
-                    res.render('Menu' , { Error : Error, Name : global.Name })
+                    var Message = "Wpłacono"
+                    res.render('Menu' , { Message : Message, Name : global.Name })
                 }
             }
         })
@@ -105,23 +107,24 @@ app.post("/menu", urlencodedParser, async (req, res) => {
         dbConnection.execute('Select Balance from accounts WHERE AccountOwnerID = ?', [global.AccountOwnerID])
         .then(([rows]) => {
             if(rows.length == 0){
-                console.log("Nie Można Wyświetlić Danych")
+                var Message = "Nie Można Wyświetlić Danych"
+                res.render('Menu' , { Message : Message, Name : global.Name })
             }
             else{
                 if(req.body.amount <= 0){
-                    var Error = "Podaj Kwotę większą od 0"
-                    res.render('Menu' , { Error : Error, Name : global.Name })
+                    var Message = "Podaj Kwotę większą od 0"
+                    res.render('Menu' , { Message : Message, Name : global.Name })
                 }
                 else{
                     var New_Balance = rows[0].Balance - req.body.amount 
                     if(New_Balance <= req.body.amount){
-                        var Error = "Nie Masz Tyle Środków"
-                        res.render('Menu' , { Error : Error, Name : global.Name })
+                        var Message = "Nie Masz Tyle Środków"
+                        res.render('Menu' , { Message : Message, Name : global.Name })
                     }
                     else{
                         dbConnection.execute('UPDATE accounts SET Balance = ? WHERE AccountOwnerID = ?', [New_Balance, global.AccountOwnerID])
-                        var Error = "Wypłacono"
-                        res.render('Menu' , { Error : Error, Name : global.Name })
+                        var Message = "Wypłacono"
+                        res.render('Menu' , { Message : Message, Name : global.Name })
                     }
                 }
             }
@@ -131,11 +134,12 @@ app.post("/menu", urlencodedParser, async (req, res) => {
         dbConnection.execute('Select Balance from accounts WHERE AccountOwnerID = ?', [global.AccountOwnerID])
         .then(([rows]) => {
             if(rows.length == 0){
-                console.log("Nie Można Wyświetlić Danych")
+                var Message = "Nie Można Wyświetlić Salda"
+                res.render('Menu' , { Message : Message, Name : global.Name })
             }
             else{
-                var Error = "Twoje Saldo To: " + rows[0].Balance
-                res.render('Menu' , { Error : Error, Name : global.Name })
+                var Message = "Twoje Saldo To: " + rows[0].Balance
+                res.render('Menu' , { Message : Message, Name : global.Name })
             }
         })
     }
@@ -143,14 +147,15 @@ app.post("/menu", urlencodedParser, async (req, res) => {
         dbConnection.execute('Select FullName from clients')
         .then(([rows]) => {
             if(rows.length == 0){
-                console.log("Nie Można Wyświetlić Danych")
+                var Message = "Nie Można Wyświetlić Danych"
+                res.render('Menu' , { Message : Message, Name : global.Name })
             }
             else{
                 for (let i = 0; i < rows.length; i++) {
                     console.log(rows[i].FullName)
                 }
-                var Error = "Lista Klientów wyświetlona w konsoli"
-                res.render('Menu' , { Error : Error, Name : global.Name })
+                var Message = "Lista Klientów wyświetlona w konsoli"
+                res.render('Menu' , { Message : Message, Name : global.Name })
             }
         })
     }
@@ -158,22 +163,23 @@ app.post("/menu", urlencodedParser, async (req, res) => {
         dbConnection.execute('Select * from accounts WHERE AccountOwnerID = ?', [global.AccountOwnerID])
         .then(([rows]) => {
             if(rows.length == 0){
-                console.log("Nie Można Wyświetlić Danych")
+                var Message = "Nie Masz Żadnych Kont"
+                res.render('Menu' , { Message : Message, Name : global.Name })
             }
             else{
                 console.log(rows)
-                var Error = "Lista Klientów wyświetlona w konsoli"
-                res.render('Menu' , { Error : Error, Name : global.Name })
+                var Message = "Identyfikator Klienta: " + rows[0].AccountOwnerID + "\nNumer Konta: " + rows[0].AccountNumber + "\nPin: " + rows[0].AccountPin + "\nSaldo: " + rows[0].Balance;
+                res.render('Menu' , { Message : Message, Name : global.Name })
             }
         })
     }
     else if (req.body.choice == "Wyloguj") {
         var Logout = "Wylogowano Poprawnie"
-        res.render('bank' , { Error : Logout })
+        res.render('bank' , { Message : Logout })
     }
     else{
-        var Error = "Brak Wybranej Opcji"
-        res.render('Menu' , { Error : Error, Name : global.Name })
+        var Message = "Brak Wybranej Opcji"
+        res.render('Menu' , { Message : Message, Name : global.Name })
     }
     } 
     catch (error) {
