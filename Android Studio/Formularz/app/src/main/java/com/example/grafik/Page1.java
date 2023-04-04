646e68f3e36088d93a99a864bd8c2725cc76cd0c
Kuba <jakubsobczyk2004@wp.pl>
2023-04-04 13:54:52 +0200

    Update

--- Android Studio/Formularz/app/src/main/java/com/example/grafik/Page1.java
+++ Android Studio/Formularz/app/src/main/java/com/example/grafik/Page1.java
@@ -4,12 +4,16 @@ package com.example.grafik;
         import android.app.AlertDialog;
         import android.app.DatePickerDialog;
         import android.content.Intent;
+        import android.database.Cursor;
+        import android.database.sqlite.SQLiteDatabase;
+        import android.database.sqlite.SQLiteStatement;
         import android.os.Bundle;
         import android.provider.Settings;
         import android.view.View;
         import android.widget.Button;
         import android.widget.CheckBox;
         import android.widget.DatePicker;
+        import android.widget.EditText;
         import android.widget.Spinner;
 
         import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -22,7 +26,17 @@ public class Page1 extends AppCompatActivity implements View.OnClickListener
 {
     private DatePickerDialog datePickerDialog;
     private Button dateButton;
-    private int Date;
+    private String date;
+    private Integer id = 1;
+    private String name;
+    private String surname;
+    private String gender;
+    private String Date;
+    private String state;
+    private String postcode;
+    private String city;
+    private String text;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -51,21 +65,56 @@ public class Page1 extends AppCompatActivity implements View.OnClickListener
 
             case R.id.register:
                 TextInputLayout Name = findViewById(R.id.name);
-                String name = String.valueOf(Name.getEditText().getText());
+                name = String.valueOf(Name.getEditText().getText());
                 TextInputLayout SurName = findViewById(R.id.name);
-                String surname = String.valueOf(SurName.getEditText().getText());
+                surname = String.valueOf(SurName.getEditText().getText());
                 CheckBox men = (CheckBox)findViewById(R.id.gender_men);
                 CheckBox women = (CheckBox)findViewById(R.id.gender_women);
                 if(men.isChecked()){
-                    String gender = "Men";
-                    dateButton.setText("Men");
+                    gender = "Mężczyzna";
                 }
                 else if(women.isChecked()){
-                    String gender = "Women";
-                    dateButton.setText("Women");
+                    gender = "Kobieta";
                 }
-
+                Date = date;
+                Spinner mySpinner = (Spinner) findViewById(R.id.state);
+                state = mySpinner.getSelectedItem().toString();
+                TextInputLayout PostCode = findViewById(R.id.postcode);
+                postcode = String.valueOf(Name.getEditText().getText());
+                TextInputLayout City = findViewById(R.id.city);
+                city = String.valueOf(SurName.getEditText().getText());
+                EditText TextArea = findViewById(R.id.textArea);
+                text = String.valueOf(TextArea.getText());
+                Datebase();
         }
+    }
+
+    public void Datebase(){
+        id++;
+        db = openOrCreateDatabase("Test", MODE_PRIVATE, null);
+        String sqlDB = "CREATE TABLE IF NOT EXISTS KLIENCI (Id INTEGER, Name VARCHAR, Surname VARCHAR, Gender VARCHAR, State VARCHAR, PostCode VARCHAR, City VARCHAR, Text VARCHAR)";
+        db.execSQL(sqlDB);
+        String sqlCount = "SELECT count(*) FROM STUDENCI";
+        Cursor cursor = db.rawQuery(sqlCount, null);
+        cursor.moveToFirst();
+        int ilosc = cursor.getInt(0);
+        cursor.close();
+        if (ilosc == 0) {
+            String sqlStudent = "INSERT INTO STUDENCI VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+            SQLiteStatement statement = db.compileStatement(sqlStudent);
+
+            statement.bindLong(1, id);
+            statement.bindString(2, name);
+            statement.bindString(3, surname);
+            statement.bindString(4, gender);
+            statement.bindString(5, state);
+            statement.bindString(6, postcode);
+            statement.bindString(7, city);
+            statement.bindString(8, text);
+            statement.executeInsert();
+        }
+
+
     }
 
     private String getTodaysDate()
@@ -85,9 +134,7 @@ public class Page1 extends AppCompatActivity implements View.OnClickListener
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day)
             {
-                month = month + 1;
-                String date = makeDateString(day, month, year);
-                Settings.Global. = date;
+                date = makeDateString(day, month+1, year);
                 dateButton.setText(date);
             }
         };
@@ -105,38 +152,37 @@ public class Page1 extends AppCompatActivity implements View.OnClickListener
 
     private String makeDateString(int day, int month, int year)
     {
-        return getMonthFormat(month) + " " + day + " " + year;
+        return day + " " + getMonthFormat(month) + " " + year;
     }
 
     private String getMonthFormat(int month)
     {
         if(month == 1)
-            return "JAN";
+            return "STYCZEŃ";
         if(month == 2)
-            return "FEB";
+            return "LUTY";
         if(month == 3)
-            return "MAR";
+            return "MARZEC";
         if(month == 4)
-            return "APR";
+            return "KWIECIEŃ";
         if(month == 5)
-            return "MAY";
+            return "MAJ";
         if(month == 6)
-            return "JUN";
+            return "CZERWIEC";
         if(month == 7)
-            return "JUL";
+            return "LIPIEC";
         if(month == 8)
-            return "AUG";
+            return "SIERPIEŃ";
         if(month == 9)
-            return "SEP";
+            return "WRZESIEŃ";
         if(month == 10)
-            return "OCT";
+            return "PAŹDZIERNIK";
         if(month == 11)
-            return "NOV";
+            return "LISTOPAD";
         if(month == 12)
-            return "DEC";
+            return "GRUDZIEŃ";
 
-        //default should never happen
-        return "JAN";
+        return "BŁĄD";
     }
 
     public void openDatePicker(View view)
--- Android Studio/Formularz/app/src/main/java/com/example/grafik/Page2.java
+++ Android Studio/Formularz/app/src/main/java/com/example/grafik/Page2.java
@@ -3,9 +3,12 @@ package com.example.grafik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -15,6 +18,7 @@ import java.util.Set;
 
 
 public class Page2 extends AppCompatActivity implements View.OnClickListener  {
+    SQLiteDatabase db;
     ListView days = null;
     Set<String> dates = new HashSet<String>();
     ArrayList<String> dates2 = new ArrayList<String>();
@@ -28,13 +32,31 @@ public class Page2 extends AppCompatActivity implements View.OnClickListener  {
         ((FloatingActionButton) findViewById(R.id.menuButton)).setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.menuButton:
-                Intent myIntent = new Intent(this, MainActivity.class);
-                startActivity(myIntent);
-                break;
+    public void onClick(View view) {
+        ArrayList<String> wyniki = new ArrayList<String>();
+        Cursor c = db.rawQuery("SELECT Id, Name, Surname FROM KLIENCI", null);
+        if (c.moveToFirst()) {
+            do {
+                int id = c.getInt(c.getColumnIndex("Id"));
+                String imie = c.getString(c.getColumnIndex("Imie"));
+                String nazwisko =
+                        c.getString(c.getColumnIndex("Nazwisko"));
+                wyniki.add("Id: " + id + ", Imię: " + imie + ",Nazwisko: " + nazwisko);
+            } while (c.moveToNext());
         }
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, wyniki);
+        listView.setAdapter(adapter);
+        c.close();
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.menuButton:
+//                Intent myIntent = new Intent(this, MainActivity.class);
+//                startActivity(myIntent);
+//                break;
+//        }
+//    }
 }
