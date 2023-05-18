bc718dd64bfcea0c5a704b5495c85ff9d68df4e5
kubaxd864 <jakubsobczyk484@gmail.com>
2023-05-18 19:34:41 +0200

    Update

--- React/Aplikacje_Desktopowe/Guess_Number_Desktop/index.js
+++ React/Aplikacje_Desktopowe/Guess_Number_Desktop/index.js
@@ -1,14 +1,14 @@
-// const electron = require('electron');
-// const { app, BrowserWindow } = electron;
-// let mainWindow;
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+let mainWindow;
 
-// app.on('ready', () => {
-//     mainWindow = new BrowserWindow({
-//         webPreferences: {
-//             nodeIntegration: true,
-//             contextIsolation: false
-//         }
-//     });
-//     mainWindow.loadURL(`${app.getAppPath()}\\build\\index.html`);
-// });
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+    mainWindow.loadURL(`${app.getAppPath()}\\build\\index.html`);
+});
 
--- React/Aplikacje_Desktopowe/Guess_Number_Desktop/src/App.js
+++ React/Aplikacje_Desktopowe/Guess_Number_Desktop/src/App.js
@@ -1,25 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [number, setNumber] = useState();
+  const [randomNumber, setRandomNumber] = useState(0);
+  const [message, setMessage] = useState('');
+  const [count, setCount] = useState(10);
+
+  const handleSubmit = (e) => {
+    setRandomNumber(Math.floor(Math.random() * count));
+    if (randomNumber == number) {
+      setMessage('Gratulacje! Zgadłeś liczbę!');
+    } else if (randomNumber < number) {
+      setMessage('Za dużo! Spróbuj jeszcze raz!');
+    } else if (randomNumber > number) {
+      setMessage('Za mało! Spróbuj jeszcze raz!');
+    } else {
+      setMessage('Wprowadź liczbę!');
+    }
+  };
 
   return (
     <div className="App">
       <div className='container'>
-        <h1>Pobierz Datę W Wybranym Formacie</h1>
-        <h3 className='Info'>Wybierz format daty</h3>
-        <select>
-          <option className='First' value=""></option>
-          <option className='Eu' value="dd/mm/yyyy">Eu date Format</option>
-          <option className='Us' value="mm/dd/yyyy">Us date Format</option>
-          <option className='China' value="yyyy/mm/dd">Chinese date Format</option>
+        <h1>Zgadnij Randomową Liczbę !</h1>
+        <h3 className='Info'>Wybierz zakres liczb</h3>
+        <select value={count} onChange={e=>setCount(e.target.value)}>
+          <option className='Word_count' value="10">Do 1 do 10</option>
+          <option className='Word_count' value="100">Do 1 do 100</option>
+          <option className='Word_count' value="1000">Do 1 do 1000</option>
+          <option className='Word_count' value="10000">Do 1 do 10000</option>
+          <option className='Word_count' value="100000">Do 1 do 100000 </option>
         </select>
-        <button className='Pick'>Pobierz Datę</button>
-        <p className='Message'>Data W Wybranym Formacie:</p>
-        <p className='Date'></p>
+        <h3 className='Info'>Wprowadź Liczbę</h3>
+        <input type="number" className='Input_Number' onChange={e => setNumber(e.target.value)} />
+        <button type="submit" className='Pick' onClick={e => handleSubmit(e.target.value)}>Graj!</button>
+        <h2>{message}</h2>
       </div>
     </div>
   );
-}
+};
 
 export default App;
